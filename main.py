3019f7b31486d77b192d5da6e69837570d39d298
Abdullah Bin Tahir <abdullahbintahir42@gmail.com>
2025-08-23 15:32:08 +0500

    making streamlit Application

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-def main():
-    print("Hello from equitynewssearchtool!")
+import os
+import streamlit as st
+import pickle
+import time
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains import RetreivalQAWithSourcesChain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import UnstructuredURLLoader
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
-if __name__ == "__main__":
-    main()
+st.title("News Search Tool📈")
+st.sidebar.title("New Article URLS")
+
+
+for i in range(3):
+    url = st.sidebar.text_input(f"Article URL {i + 1}")
+
+process_url_clicked = st.sidebar.button("Process URLs")
+
+if process_url_clicked:
+    pass
